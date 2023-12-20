9d877b53bf2f96c8fbb4233f9f6b5e8d28b81f52
vladislavserkov <vladislavserkovv@gmail.com>
2023-12-20 13:14:41 +0300

    fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,20 +1,26 @@
 public class Calculator {
+
     public int add(int a, int b){
         return a+b;
     }
+
     public int dif(int a, int b){
         return a - b;
     }
+
     public int div(int a, int b){
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed.");
         }
         return a / b;
     }
+
     public int times(int a, int b){
         return a * b;
     }
+
     public int solver(){
         return 1;
     }
+
 }
